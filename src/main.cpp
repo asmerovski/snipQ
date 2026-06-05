ae3340474fa561a3b0b0391ce2bb5df7b7da5e80
asmerovski <asmerovski@gmail.com>
2026-06-05 12:10:23 +0200

    small fixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,22 @@
 #include "theme.h"
 
 int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
+
+    QApplication::setHighDpiScaleFactorRoundingPolicy(
+    Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+
+QApplication app(argc, argv);
 
     app.setApplicationName("snipQ");
     app.setOrganizationName("snipQ");
     app.setApplicationVersion("1.0.0");
 
     // Use Fusion base style for cross-platform consistency
-    app.setStyle(QStyleFactory::create("Fusion"));
+    // app.setStyle(QStyleFactory::create("Fusion"));
 
     // Apply our dark theme
-    app.setStyleSheet(Theme::StyleSheet);
+    // app.setStyleSheet(Theme::StyleSheet);
 
-    // High-DPI
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-    app.setHighDpiScaleFactorRoundingPolicy(
-        Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-#endif
 
     MainWindow win;
     win.show();
